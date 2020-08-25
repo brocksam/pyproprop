94e5935e71c0a0a5bd517a2386c0dfcfdb633ed9
Sam Brockie <sgb39@cam.ac.uk>
2020-08-25 14:03:59 +0100

    Create basic unit tests module for named iterables

---
+++ tests/test_named_iterable.py
@@ -0,0 +1,43 @@
+import string
+
+from hypothesis import given
+import hypothesis.strategies as st
+import pytest
+
+from pyproprop import named_iterable
+
+
+@pytest.mark.usefixtures("_named_iterable_fixture")
+class TestNamedIterable:
+
+    @pytest.fixture(autouse=True)
+    def _named_iterable_fixture(self):
+        """Simple fixture setting up an named iterables."""
+        self.single_named_iter = named_iterable(1, named_keys=["x"])
+        self.double_named_iter = named_iterable([1, 2], named_keys=["x", "y"])
+
+    def test_dot_indexing(self):
+        """Check that value- and dot-indexing are equivalent."""
+        assert self.single_named_iter.x == self.single_named_iter[0]
+        assert self.double_named_iter.x == self.double_named_iter[0]
+        assert self.double_named_iter.y == self.double_named_iter[1]
+
+
+@given(keys=st.iterables(st.text(alphabet=string.ascii_letters, min_size=1), min_size=1),
+       values=st.iterables(st.one_of(st.floats(), st.integers(), st.booleans()), min_size=1))
+def test_named_iterable_creation_as_mapping(keys, values):
+    _ = named_iterable(dict(zip(keys, values)))
+
+
+@given(iterable=st.just(iter(['A'])))
+# @given(iterable=st.iterables(st.text(alphabet=string.ascii_letters, min_size=1), min_size=1, unique=True))
+def test_named_iterable_creation_data_object(iterable):
+    _ = named_iterable(iterable)
+
+
+@given(keys=st.iterables(st.text(alphabet=(string.digits + string.punctuation + string.whitespace), min_size=1), min_size=1),
+       values=st.iterables(st.one_of(st.floats(), st.integers(), st.booleans()), min_size=1))
+def test_invalid_identifiers(keys, values):
+    """Invalid identifiers raise ValueError."""
+    with pytest.raises(ValueError):
+        _ = named_iterable(dict(zip(keys, values)))
