47414903b2f93156861403cb4947ab1e43299f74
Sam Brockie <sgb39@cam.ac.uk>
2020-08-25 11:46:24 +0100

    Add Sphinx extensions including autodoc, todo, napoleon etc.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,16 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
