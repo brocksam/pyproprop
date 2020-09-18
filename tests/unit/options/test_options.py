754835bb079fd8bae5ec80ddaf027fc1204cca89
Restyled.io <commits@restyled.io>
2020-09-18 13:49:49 +0000

    Restyled by yapf

--- tests/unit/options/test_options.py
+++ tests/unit/options/test_options.py
@@ -40,7 +40,7 @@ def test_single_option_instantiation():
     """:obj:`Options` with single option can be instantiated and initialised
     correctly."""
     options = Options(OPTION_1_KEYWORD)
-    assert options.options == (OPTION_1_KEYWORD,)
+    assert options.options == (OPTION_1_KEYWORD, )
     assert options.default == OPTION_1_KEYWORD
     assert options.unsupported == ()
 
@@ -68,8 +68,7 @@ def test_value_error_for_default_option_not_option():
     expected_error_msg = re.escape(
         "`'option_5'` is not a valid choice of default as it is not an "
         "option. Please choose one of: `'option_1'`, `'option_2'` or "
-        "`'option_3'`"
-    )
+        "`'option_3'`")
     with pytest.raises(ValueError, match=expected_error_msg):
         _ = Options(options_tuple, default=OPTION_5_KEYWORD)
 
@@ -79,8 +78,7 @@ def test_value_error_for_single_unsupported_option_not_option():
     expected_error_msg = re.escape(
         "`'option_5'` is not a valid choice of unsupported option as it is "
         "not an option. Please choose from: `'option_1'`, `'option_2'` and "
-        "`'option_3'`"
-    )
+        "`'option_3'`")
     with pytest.raises(ValueError, match=expected_error_msg):
         _ = Options(options_tuple, unsupported=OPTION_5_KEYWORD)
 
@@ -90,14 +88,15 @@ def test_value_error_for_multiple_unsupported_option_not_option():
     expected_error_msg = re.escape(
         "`'option_4'` and `'option_5'` are not a valid choices of unsupported "
         "options as they are not options. Please choose from: `'option_1'`, "
-        "`'option_2'` and `'option_3'`"
-    )
+        "`'option_2'` and `'option_3'`")
     with pytest.raises(ValueError, match=expected_error_msg):
-        _ = Options(options_tuple, unsupported=(OPTION_4_KEYWORD, OPTION_5_KEYWORD))
+        _ = Options(options_tuple,
+                    unsupported=(OPTION_4_KEYWORD, OPTION_5_KEYWORD))
 
 
 def test_value_error_single_option_unsupported():
-    expected_error_msg = re.escape("All options (`'option_1'`) are unsupported.")
+    expected_error_msg = re.escape(
+        "All options (`'option_1'`) are unsupported.")
     with pytest.raises(ValueError, match=expected_error_msg):
         _ = Options(OPTION_1_KEYWORD, unsupported=OPTION_1_KEYWORD)
 
@@ -105,7 +104,7 @@ def test_value_error_single_option_unsupported():
 def test_value_error_multiple_options_all_unsupported():
     options_tuple = (OPTION_1_KEYWORD, OPTION_2_KEYWORD, OPTION_3_KEYWORD)
     expected_error_msg = expected_error_msg = re.escape(
-        "All options (`'option_1'`, `'option_2'` and `'option_3'`) are " "unsupported."
-    )
+        "All options (`'option_1'`, `'option_2'` and `'option_3'`) are "
+        "unsupported.")
     with pytest.raises(ValueError, match=expected_error_msg):
         _ = Options(options_tuple, unsupported=options_tuple)
