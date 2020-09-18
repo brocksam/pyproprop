df18d357285972eee1f54636204ddfd27ccf0bbb
Sam Brockie <sgb39@cam.ac.uk>
2020-09-18 13:44:46 +0100

    Add tests for using Options with processed_property

--- tests/unit/options/test_processed_property_with_option.py
+++ tests/unit/options/test_processed_property_with_option.py
@@ -19,7 +19,7 @@ import re
 
 import pytest
 
-from pyproprop import processed_property
+from pyproprop import Options, processed_property
 
 
 OPTION_1_KEYWORD = "option_1"
@@ -27,6 +27,25 @@ OPTION_2_KEYWORD = "option_2"
 OPTION_3_KEYWORD = "option_3"
 OPTION_4_KEYWORD = "option_4"
 OPTION_5_KEYWORD = "option_5"
+
+options_1_to_5 = (OPTION_1_KEYWORD,
+                  OPTION_2_KEYWORD,
+                  OPTION_3_KEYWORD,
+                  OPTION_4_KEYWORD,
+                  OPTION_5_KEYWORD,
+                  )
+options_3_to_5 = (OPTION_3_KEYWORD,
+                  OPTION_4_KEYWORD,
+                  OPTION_5_KEYWORD,
+                  )
+
+single_option = Options(OPTION_1_KEYWORD)
+multiple_options = Options(options_1_to_5)
+multiple_options_single_unsupported = Options(options_1_to_5,
+                                              unsupported=OPTION_5_KEYWORD)
+multiple_options_multiple_unsupported = Options(options_1_to_5,
+                                                unsupported=options_3_to_5)
+
 
 
 class ClassWithOptionProperties:
@@ -40,47 +59,27 @@ class ClassWithOptionProperties:
         "one_option_prop",
         description="option property with a single option",
         type=str,
-        options=(OPTION_1_KEYWORD, )
+        options=single_option,
     )
     mul_option_prop = processed_property(
         "mul_option_prop",
         description="option property with multiple options",
         type=str,
-        options=(OPTION_1_KEYWORD,
-                 OPTION_2_KEYWORD,
-                 OPTION_3_KEYWORD,
-                 OPTION_4_KEYWORD,
-                 OPTION_5_KEYWORD,
-                 )
+        options=multiple_options,
     )
     one_unsupported_option_prop = processed_property(
         "one_unsupported_option_prop",
         description=("option property with multiple options and a single "
                      "unsupported option"),
         type=str,
-        options=(OPTION_1_KEYWORD,
-                 OPTION_2_KEYWORD,
-                 OPTION_3_KEYWORD,
-                 OPTION_4_KEYWORD,
-                 OPTION_5_KEYWORD,
-                 ),
-        unsupported_options=(OPTION_5_KEYWORD, ),
+        options=multiple_options_single_unsupported,
     )
     mul_unsupported_option_prop = processed_property(
         "mul_unsupported_option_prop",
         description=("option property with multiple options and multiple "
                      "unsupported options"),
         type=str,
-        options=(OPTION_1_KEYWORD,
-                 OPTION_2_KEYWORD,
-                 OPTION_3_KEYWORD,
-                 OPTION_4_KEYWORD,
-                 OPTION_5_KEYWORD,
-                 ),
-        unsupported_options=(OPTION_3_KEYWORD,
-                             OPTION_4_KEYWORD,
-                             OPTION_5_KEYWORD,
-                             ),
+        options=multiple_options_multiple_unsupported,
     )
 
     def __init__(self,
@@ -144,51 +143,6 @@ def test_missing_option_kwarg_with_unsupported_option():
             )
 
 
-def test_no_option_with_unsupported_option():
-    """"""
-    expected_error_msg = re.escape(
-        f"`one_all_unsupported_option_prop` does not have any supported "
-        f"options. Check unsupported options are valid options: `'option_1'`.")
-    with pytest.raises(ValueError, match=expected_error_msg):
-
-        class ClassWithSingleOptionAllUnsupportedOptionProperties:
-
-            one_all_unsupported_option_prop = processed_property(
-                "one_all_unsupported_option_prop",
-                type=str,
-                options=(),
-                unsupported_options=(OPTION_1_KEYWORD, ),
-            )
-
-
-def test_multiple_option_all_are_unsupported_option():
-    """"""
-    expected_error_msg = re.escape(
-        f"`mul_all_unsupported_option_prop` does not have any supported "
-        f"options from: `'option_1'`, `'option_2'`, `'option_3'`, "
-        f"`'option_4'` and `'option_5'`.")
-    with pytest.raises(ValueError, match=expected_error_msg):
-
-        class ClassWithMultipleOptionAllUnsupportedOptionProperties:
-
-            mul_all_unsupported_option_prop = processed_property(
-                "mul_all_unsupported_option_prop",
-                type=str,
-                options=(OPTION_1_KEYWORD,
-                         OPTION_2_KEYWORD,
-                         OPTION_3_KEYWORD,
-                         OPTION_4_KEYWORD,
-                         OPTION_5_KEYWORD,
-                         ),
-                unsupported_options=(OPTION_1_KEYWORD,
-                                     OPTION_2_KEYWORD,
-                                     OPTION_3_KEYWORD,
-                                     OPTION_4_KEYWORD,
-                                     OPTION_5_KEYWORD,
-                                     ),
-            )
-
-
 def test_correct_initialisation_class_with_option_properties(test_fixture):
     assert test_fixture.one_option_prop == OPTION_1_KEYWORD
     assert test_fixture.mul_option_prop == OPTION_1_KEYWORD
@@ -201,25 +155,3 @@ def test_raises_value_error_with_invalid_option(test_fixture):
         test_fixture.one_option_prop = OPTION_2_KEYWORD
     with pytest.raises(ValueError):
         test_fixture.one_unsupported_option_prop = OPTION_5_KEYWORD
-
-
-def test_option_creation_from_dict_keys():
-
-    class ClassWithMultipleOptionAllUnsupportedOptionProperties:
-
-        option_from_dict_keys_prop = processed_property(
-            "option_from_dict_keys_prop",
-            type=str,
-            options={OPTION_1_KEYWORD: None,
-                     OPTION_2_KEYWORD: None,
-                     OPTION_3_KEYWORD: None,
-                     OPTION_4_KEYWORD: None,
-                     OPTION_5_KEYWORD: None,
-                     }.keys(),
-            unsupported_options=(OPTION_5_KEYWORD, ),
-        )
-
-    def __init__(self):
-        self.option_from_dict_keys_prop = OPTION_1_KEYWORD
-
-    _ = ClassWithMultipleOptionAllUnsupportedOptionProperties()
