9680d7bd18b1eefb65bc881899a90d3102e16ffb
Sam Brockie <sgb39@cam.ac.uk>
2020-11-27 12:28:08 +0000

    Bump version to 0.4.4

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ def get_contents_from_file(filepath, by_line=False, strip=""):
 
 
 PACKAGE_NAME = "pyproprop"
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 AUTHOR = "Sam Brockie"
 AUTHOR_EMAIL = "sambrockie@icloud.com"
 DESCRIPTION = ("Package for aiding writing classes with lots of similar "
