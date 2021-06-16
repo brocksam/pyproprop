62d1025cf755c02ceffcb14b475df4fea60e3ccd
Sam Brockie <sgb39@cam.ac.uk>
2021-06-16 11:27:25 +0100

    Add file based on example from PyScaffold

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup file for Pyproprop.
+
+This is based on the example from PyScaffold (https://pyscaffold.org/).
+`setup.cfg` is used to configure the project.
+
+"""
+
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:
+        msg = ("\n\nAn error occurred while building the project, "
+               "please ensure you have the most updated version of setuptools, "
+               "setuptools_scm and wheel with:\n"
+               "   pip install -U setuptools setuptools_scm wheel\n\n"
+        print(msg)
+        raise
