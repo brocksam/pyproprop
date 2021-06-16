15d1c957512d423d36df9939ad7335c4d139b9ad
Sam Brockie <sgb39@cam.ac.uk>
2021-06-16 12:30:39 +0100

    Add Exception to bare except

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ This is based on the example from PyScaffold (https://pyscaffold.org/).
 
 from setuptools import setup
 
-
 if __name__ == "__main__":
     try:
         setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:
-        msg = ("\n\nAn error occurred while building the project, "
-               "please ensure you have the most updated version of setuptools, "
-               "setuptools_scm and wheel with:\n"
-               "   pip install -U setuptools setuptools_scm wheel\n\n"
+    except Exception:
+        msg = (
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
         print(msg)
         raise
